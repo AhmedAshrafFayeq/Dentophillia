f76aacca163e245ee521d99165b1654b2101b409
Ahmed Fayeq <ahmed.fayek@vodafone.com>
2021-12-02 10:47:34 +0200

    add pod and alamofire

---
+++ dentophilia/Pods/Target Support Files/Pods-dentophilia-dentophiliaUITests/Pods-dentophilia-dentophiliaUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dentophilia_dentophiliaUITests : NSObject
+@end
+@implementation PodsDummy_Pods_dentophilia_dentophiliaUITests
+@end
---
+++ dentophilia/Pods/Target Support Files/Pods-dentophilia/Pods-dentophilia-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_dentophilia : NSObject
+@end
+@implementation PodsDummy_Pods_dentophilia
+@end
