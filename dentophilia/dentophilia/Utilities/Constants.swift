e3d0151c47cad0215e3fefe7c88e7149678a5944
Ahmed Fayeq <ahmedashraffayeq@gmail.com>
2022-01-14 17:38:08 +0200

    add constants

--- dentophilia/dentophilia/Utilities/Constants.swift
+++ dentophilia/dentophilia/Utilities/Constants.swift
@@ -20,3 +20,11 @@ struct TodoRequests: URLConvertible {
         return URL(string: "https://jsonplaceholder.typicode.com/todos/1")!
     }
 }
+
+enum StatusCodes:Int{
+     case ok = 200 // success
+     case unknown = 0 // unknown errors
+     case error400 = 400 // Error
+     case noNetwork = 1 // no internet
+     case databaseError = 12345 // DB Error code
+}
